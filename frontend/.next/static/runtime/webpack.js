8839d8a54a9b112ff4fd59235d650ef5278522f9
jack gaarkeuken <jack.gaarkeuken@gmail.com>
2019-05-04 02:28:36 -0400

    prod url

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f97a445d5e76cb558d1a";
+/******/ 	var hotCurrentHash = "f92bc7914f696f2768ef";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
